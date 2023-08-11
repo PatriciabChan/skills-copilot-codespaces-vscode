28f63f45a3f4acdce7c821f10fef568135cddb1e
PatriciabChan <patriciab_chan@hotmail.com>
2023-08-11 18:54:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var memberButton = document.getElementById("memberButton");
+  var skillsButton = document.getElementById("skillsButton");
+  member.style.display = "block";
+  skills.style.display = "none";
+  memberButton.style.backgroundColor = "#f2f2f2";
+  skillsButton.style.backgroundColor = "#e6e6e6";
+}
